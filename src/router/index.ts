50febc43255f79611625b978267da4086d5d967b
Archimedes Trajano <developer@trajano.net>
2017-11-03 07:59:05 -0400

    added initial routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,30 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
+    {
+      path: '/cb',
+      name: 'Callback',
+      meta: {
+        permit_all: true
+      },
+      component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      meta: {
+        permit_all: true
+      },
+      component: HelloWorld
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: HelloWorld
+    },
     {
       path: '/',
       name: 'Hello',
@@ -13,3 +35,14 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (sessionStorage.getItem("access_token") == null && to.meta.permit_all) {
+    sessionStorage.setItem("requested_uri", to.fullPath)
+    next("/login")
+  } else {
+    next()
+  }
+})
+
+export default router
