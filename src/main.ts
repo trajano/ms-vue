ed370c11206f2f29b4f1e5fa612922831861e0cd
Archimedes Trajano <arch@trajano.net>
2017-11-03 14:14:37 -0400

    Aborting ts branch

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import OfficeUIFabricVue from 'office-ui-fabric-vue'
+import 'office-ui-fabric-vue/dist/index.css'
 
 Vue.config.productionTip = false
+Vue.use(OfficeUIFabricVue);
 
 /* eslint-disable no-new */
 new Vue({
